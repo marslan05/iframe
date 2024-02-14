860b001eb649f3bedcf1e631f14423a7fcf296fc
Arslan Mukhtar <arslan@gecora.com>
2024-02-14 05:12:00 +0500

    Trying different approaches for desktop, tablets and mobile devices

--- src/app/page.js
+++ src/app/page.js
@@ -55,7 +55,7 @@ export default function Home() {
 
   const handleResize = () => {
     // Get the current width of the div
-    const currentWidth = document.getElementById("video").clientWidth();
+    const currentWidth = document.getElementById("video").clientWidth;
     alert("currentWidth ", currentWidth);
     setWidth(currentWidth);
   };
